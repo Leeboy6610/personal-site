2ed42994002623e74fc26b674104c7e53d8b9779
Leo Schultz <schultz.leo@gmail.com>
2014-09-23 16:35:42 -0700

    PHP Blog "Post" API

--- api/config.php
+++ api/config.php
@@ -2,7 +2,20 @@
 $dbhost = 'localhost';
 $dbuser = 'main';
 $dbpass = 'password';
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if (!$conn)
+{
+    die('Could not connect: ' . mysql_error());
+}
+//echo 'MySQL Connected successfully'."<BR>";
+
 $dbname = 'personal';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
-mysql_select_db($dbname);
+$db_selected = mysql_select_db($dbname, $conn);
+if (!$db_selected)
+{
+    die ("Can\'t use test_db : " . mysql_error());
+}
+//echo 'Connected to database successfully'."<BR>";
+
 ?>
